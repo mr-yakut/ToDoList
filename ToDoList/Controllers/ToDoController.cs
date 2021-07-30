c04aabb5f041e7c173a87c1e488b9c82f4250985
Muhammed Yakut <memoaskan@gmail.com>
2021-07-30 16:02:00 +0300

    Yapılacaklar Listesi

---
+++ ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Infrastructure;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext context;
+
+        public ToDoController(ToDoContext context)
+        {
+            this.context = context;
+        }
+        //GET/
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.ToDolist orderby i.Id select i;
+
+            List<TodoList> todoLists = await items.ToListAsync();
+
+            return View(todoLists);
+
+        }
+        //GET/todo/create
+        public IActionResult Create() => View();
+
+        //POST /todo/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if(ModelState.IsValid)
+            {
+                context.Add(item);
+                await context.SaveChangesAsync();
+
+                TempData["Succes"] = "The item has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+
+        }
+
+        //GET /todo/edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            TodoList item = await context.ToDolist.FindAsync(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+
+        }
+
+        //POST /todo/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(item);
+                await context.SaveChangesAsync();
+
+                TempData["Succes"] = "The item has been updated!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+
+        }
+        //GET /todo/delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await context.ToDolist.FindAsync(id);
+            if (item == null)
+            {
+                TempData["Error"] = "The item does not exist!";
+            }
+            else
+            {
+                context.ToDolist.Remove(item);
+                await context.SaveChangesAsync();
+
+                TempData["Succes"] = "The item has been deleted!";
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ ToDoList/Migrations/20210528201636_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDolist",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDolist", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDolist");
+        }
+    }
+}
